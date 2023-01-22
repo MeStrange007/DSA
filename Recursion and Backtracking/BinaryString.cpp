b7f49eeb4e2be64d28007783cebbc9560e89c5f6
MeStrange007 <123303891+MeStrange007@users.noreply.github.com>
2023-01-22 19:11:03 +0530

    Edit: in printArray size error

--- Recursion and Backtracking/BinaryString.cpp
+++ Recursion and Backtracking/BinaryString.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printArr(int size,int* arr){
+void printArr(int* arr,int size){
     cout<<endl;
     for(int i=0;i<size;i++){
         cout<<arr[i];
     }
 }
 
-void printBinaryString(int n,int* arr){
+void printBinaryString(int n,int* arr,int size){
     if(n<1){
-        printArr(n,arr);
+        printArr(arr,size);
     }else{
         arr[n-1]=0;
-        printBinaryString(n-1,arr);
+        printBinaryString(n-1,arr,size);
         arr[n-1]=1;
-        printBinaryString(n-1,arr);
+        printBinaryString(n-1,arr,size);
     }
 }
 
@@ -25,5 +25,5 @@ int main(){
     cout<<"Enter length of binary string : ";cin>>size;
     cout<<endl;
     int *arr = new int[size];
-    printBinaryString(size,arr);
+    printBinaryString(size,arr,size);
 }

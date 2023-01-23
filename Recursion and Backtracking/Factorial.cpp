51445fcb83e778022cc6c228494fcafea12d5643
MeStrange007 <123303891+MeStrange007@users.noreply.github.com>
2023-01-23 17:43:26 +0530

    Added findFactorial program

---
+++ Recursion and Backtracking/Factorial.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findFact(int num){
+    if(num==1||num==0){
+        return 1;
+    }else{
+        return num*findFact(num-1);
+    }
+}
+
+int main(){
+    int num;
+    cout<<"Enter number to find factorial of : ";cin>>num;
+    cout<<endl<<findFact(num);
+}
